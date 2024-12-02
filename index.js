485bd2753e25b12bb7164d13db1bb11d94f775a2
Supravisor <153783117+Supravisor@users.noreply.github.com>
2024-12-02 14:51:05 +1300

    add api for whoami

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.use("/api/whoami", function(req, res, next) {
+  res.json( {
+    ipaddress: req.ip,
+    others: Object.keys(req)  
+  } );
+  next();
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
